f5811da0746c1d4785bbd552a19ed687d7a80590
ALi Shhoury <alishhoury78@gmail.com>
2025-06-29 16:10:48 +0300

    added start,end time to show_times

--- server/migrations/005_create_show_times_table.php
+++ server/migrations/005_create_show_times_table.php
@@ -5,6 +5,8 @@ $query = "CREATE TABLE show_times(
           id INT(11) AUTO_INCREMENT PRIMARY KEY,
           film_id INT,
           auditerium_id INT,
+          start_time INT NOT NULL;
+          end_time INT NOT NULL,
           FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
           FOREIGN KEY (auditerium_id) REFERENCES Auditeriums(id) ON DELETE CASCADE,
           )";
