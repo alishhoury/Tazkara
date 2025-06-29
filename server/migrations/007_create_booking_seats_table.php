3081f01471476ca8a61af81c744d48803d981ce3
ALi Shhoury <alishhoury78@gmail.com>
2025-06-29 17:37:00 +0300

    added seat id instead of seat number

--- server/migrations/007_create_booking_seats_table.php
+++ server/migrations/007_create_booking_seats_table.php
@@ -4,7 +4,8 @@ require_once("./connection/connection.php");
 $query = "CREATE TABLE booking_seats(
           id INT AUTO_INCREMENT PRIMARY KEY,
           booking_id INT NOT NULL,
-          seat_number VARCHAR(10) NOT NULL,
+          seat_id VARCHAR(10) NOT NULL,
+          FOREIGN KEY (seat_id) REFERENCES seats(id) ON DELETE CASCADE,
           FOREIGN KEY (booking_id) REFERENCES bookings(id) ON DELETE CASCADE
           )";
 
