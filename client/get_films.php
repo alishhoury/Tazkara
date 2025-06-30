4a907b7f6f4080be665446cb8bb7b3144f80012a
ALi Shhoury <alishhoury78@gmail.com>
2025-06-30 22:34:49 +0300

    added film card overlay and fixed vugs that are making the image strettch its container

--- client/get_films.php
+++ client/get_films.php
@@ -1,6 +1,6 @@
 <?php
 $films = [
-    ["title" => "Film 1", "image" => "assets/film1.jpg", "description" => "Description for Film 1"],
+    ["title" => "Film 1", "image" => "assets/film1.jpg", "description" => "In Karate Kid: Legends, after In Karate Kid: Legends,"],
     ["title" => "Film 2", "image" => "assets/film2.jpg", "description" => "Description for Film 2"],
     ["title" => "Film 3", "image" => "assets/film3.jpg", "description" => "Description for Film 3"]
 ];
