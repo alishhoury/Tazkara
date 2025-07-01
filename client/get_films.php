febd6c92901c687389d360345c78145a09348bec
ALi Shhoury <alishhoury78@gmail.com>
2025-07-01 23:48:58 +0300

    fixed cards taking whole screen

--- client/get_films.php
+++ client/get_films.php
@@ -1,7 +1,33 @@
 <?php
 $films = [
-    ["title" => "Film 1", "image" => "assets/film1.jpg", "description" => "In Karate Kid: Legends, after In Karate Kid: Legends,"],
-    ["title" => "Film 2", "image" => "assets/film2.jpg", "description" => "Description for Film 2"],
-    ["title" => "Film 3", "image" => "assets/film3.jpg", "description" => "Description for Film 3"]
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+    ["title" => "Stich", "image" => "assets/film1.jpg", "description" => "description"],
+
+
 ];
 echo json_encode($films);
