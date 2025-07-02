f536f5d9e0575c9314f6eb8825d15aea85029613
ALi Shhoury <alishhoury78@gmail.com>
2025-07-02 06:25:13 +0300

    added info about the film

--- client/scripts/hero.js
+++ client/scripts/hero.js
@@ -1,34 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const params = new URLSearchParams(window.location.search);
-    const filmId = params.get('id');
+document.addEventListener("DOMContentLoaded", function () {
+  const params = new URLSearchParams(window.location.search);
+  const filmId = params.get("id");
 
-    fetch("../get_films.php")
-        .then(response => response.json())
-        .then(films => {
-            const cards = document.getElementById('hero');
-            const film = films.find(f => String(f.id) === filmId);
-            if (film) {
-                let videoEmbed = '';
-                if (film.video) {
-                    const youtubeMatch = film.video.match(/(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/);
-                    if (youtubeMatch){
-                        videoEmbed = `<iframe src="https://www.youtube.com/embed/${youtubeMatch[1]}"> </iframe>`;
-                    }
-                }
-                cards.innerHTML = `
-                    <div class="hero-section">
-                    <h2>${film.title}</h2>
-                        <img src="${film.image}" alt="${film.title}">
-                        <div class = "trailer">
-                        ${videoEmbed}
-                        </div>
-                         <button>View Showtimes</button>
-                         <p>Genre:${film.genre}<p>
-                        <p>Running Time: ${film.duration}<p>
-                        <p>Release Date: ${film.realseDate}<p>
-                
-                    </div>
-                `;
-            }
-        });
+  fetch("../get_films.php")
+    .then((response) => response.json())
+    .then((films) => {
+      const cards = document.getElementById("hero");
+      const film = films.find((f) => String(f.id) === filmId);
+      if (film) {
+        let videoEmbed = "";
+        if (film.video) {
+          const youtubeMatch = film.video.match(
+            /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/
+          );
+          if (youtubeMatch) {
+            videoEmbed = `<iframe src="https://www.youtube.com/embed/${youtubeMatch[1]}"> </iframe>`;
+          }
+        }
+        cards.innerHTML = `
+    <div class="hero-section">
+        <h2>${film.title}</h2>
+        <img src="${film.image}" alt="${film.title}">
+        <div class="trailer">
+            ${videoEmbed}
+        </div>
+        <div><button>View Showtimes</button></div>
+         <div class="info">
+            <p>Genre: ${film.genre}</p>
+            <p>Running Time: ${film.duration ? film.duration : 'N/A'}</p>
+            <p>Release Date: ${film.realseDate ? film.realseDate : 'N/A'}</p>
+         </div>
+    </div>
+                         `;
+      }
+    });
 });
