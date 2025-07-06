1977e2264356420f783532b1fc6339533dd4b91f
ALi Shhoury <alishhoury78@gmail.com>
2025-07-07 01:01:40 +0300

    created find method in model

---
+++ server/models/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Model {
+
+    protected static string $table;
+    protected static string $primary_key = "id";
+
+    public static function find(mysqli $mysqli, int $id){
+        $sql = sprintf("SELECT * from %s WHERE %s = ? , $static::$table, $static::$primary_key");
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+        return $data ? new static($data) : null;
+    }
+
+
+}
