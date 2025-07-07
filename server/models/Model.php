bfa3c7e3d8ffe8ddc360ea2c397a2040ad2eabed
ALi Shhoury <alishhoury78@gmail.com>
2025-07-07 03:05:16 +0300

    created create method

--- server/models/Model.php
+++ server/models/Model.php
@@ -54,6 +54,28 @@ abstract class Model {
              return $query->execute();
         }
 
+        public static function create(mysqli $mysqli,array $fields){
+
+            $columns = implode(', ', array_keys($fields));
+            $placeholders = implode(', ', array_fill(0, count($fields), '?'));
+            $types = '';
+            foreach ($fields as $value){
+                $types = $types . (is_int($value) ? 'i' : 's');
+            }
+            $sql = sprintf("INSERT INTO %s (%s)Values (%s)", static::$table, $columns, $placeholders);
+            $query = $mysqli->prepare($sql);
+            $query->bind_param($types, ...array_values($fields));
+
+            $exuecuted = $query->execute(); 
+            if($exuecuted) {
+                $field[static::$primary_key] = $mysqli->insert_id;
+                return new static($fields);
+            }
+            return null;
+
+
+        }
+
 
 
 
