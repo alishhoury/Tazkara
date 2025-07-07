69d6d210af4a4ff0b5a7bed00d2d16c35e10d1fa
ALi Shhoury <alishhoury78@gmail.com>
2025-07-07 03:40:39 +0300

    added getters

--- server/models/User.php
+++ server/models/User.php
@@ -1,2 +1,43 @@
 <?php
-require_once("Model,php");
+require_once("Model.php");
+class User extends Model {
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $mobile;
+
+    protected static string $table = "users";
+    protected static string $primaryKey = "id";
+
+    public function __construct(array $data) {
+
+        $this->id = $data["id"];
+        $this->username = $data["username"];
+        $this->email = $data["email"];
+        $this->password = $data["password"];
+        $this->mobile = $data["mobile"] ;
+    }
+
+     public function getId():int {
+        return $this->id;
+
+    }
+        public function getName():string {
+        return $this->username;
+        
+    }
+        public function getEmail():string {
+        return $this->email;
+        
+    }
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function getMobile(): string {
+        return $this->mobile;
+    }
+
+
+}
